0c3784599d7024e38ecca9641411df2e3564a4e3
fangsheng <445317262@qq.com>
2021-02-20 11:36:21 +0800

    1

---
+++ src/Lambda01/test01.java
@@ -0,0 +1,11 @@
+package Lambda01;
+
+/**
+ * @author fangsheng
+ * @date 2021/2/20 上午11:36
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("ss");
+    }
+}
